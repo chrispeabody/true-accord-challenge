9bb091cc6a1e6cd02dbb3b99dc370b64dd996fc6
Chris Peabody <cgpzbd@mst.edu>
2021-01-17 12:15:47 -0600

    Created the 'main' function, App. Removed AppTest because unit tests of this function would be redundant and would basically end up being integration tests.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,26 @@
+import data.Debt;
+import data.DebtInfo;
+import data.Payment;
+import data.PaymentPlan;
+import utilities.APIDataRetriever;
+import utilities.DebtAnalysisService;
+
+import java.util.List;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
+        APIDataRetriever apiDataRetriever = new APIDataRetriever(
+                "https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/");
 
-        System.out.println(new App().getGreeting());
+        List<Debt> allDebts = apiDataRetriever.getAllDebts();
+        List<PaymentPlan> allPaymentPlans = apiDataRetriever.getAllPaymentPlans();
+        List<Payment> allPayments = apiDataRetriever.getAllPayments();
+
+        DebtAnalysisService debtAnalysisService = new DebtAnalysisService(allDebts, allPaymentPlans, allPayments);
+        List<DebtInfo> debtInfos = debtAnalysisService.generateDebtInfos();
+
+        for (DebtInfo debtInfo : debtInfos) {
+            debtInfo.print();
+        }
     }
 }
